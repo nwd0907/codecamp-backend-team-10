73caa161ccbdc6ac740d16bfbcbb4372fabe1304
patrryan <patangmond329@gmail.com>
2022-12-23 17:34:26 +0900

    INIT: layout for points transactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PointsTransactionsModule } from './apis/points-transactions/points-transactions.module';
 
 @Module({
-  imports: [],
+  imports: [PointsTransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
